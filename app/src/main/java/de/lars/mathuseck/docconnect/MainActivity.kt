3d6d9f482308947d013b29862586658c7b335e0c
Lars Mathuseck <lars.mathuseck@gmail.com>
2020-03-21 11:59:43 +0100

    fixed merge conflicts

--- app/src/main/java/de/lars/mathuseck/docconnect/MainActivity.kt
+++ app/src/main/java/de/lars/mathuseck/docconnect/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.lars.mathuseck.docconnect
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
